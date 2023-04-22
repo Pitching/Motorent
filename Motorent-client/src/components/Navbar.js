91d4868a0e2a2de2cea1c56fd548bd111175755e
Pitching <adchir95@gmail.com>
2023-04-22 13:23:46 -0400

    initializing the navbar

--- Motorent-client/src/components/Navbar.js
+++ Motorent-client/src/components/Navbar.js
@@ -1,10 +1,21 @@
 import './Navbar.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-      Testing!
-    </div>
+    <header className="p-3 text-bg-dark">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
+          </ul>
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+
+          </form>
+        </div>
+      </div>
+    </header>
   )
 }
 
