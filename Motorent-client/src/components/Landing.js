6263e412dcefe84a2623879e6737da7c720fe79f
Pitching <adchir95@gmail.com>
2023-05-11 14:43:39 -0400

    adding react bootstrap button

--- Motorent-client/src/components/Landing.js
+++ Motorent-client/src/components/Landing.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
 
 const Landing = () => {
-  
+
 }
