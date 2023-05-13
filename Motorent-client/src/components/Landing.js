1572131a7cf9e0e45201709ebb0e1725a19ddad7
Pitching <adchir95@gmail.com>
2023-05-13 14:45:29 -0400

    adding cards

--- Motorent-client/src/components/Landing.js
+++ Motorent-client/src/components/Landing.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
-
+import Card from 'react-bootstrap/Card';
 
 const Landing = () => {
 
