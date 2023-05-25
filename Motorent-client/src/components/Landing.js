f01e16a3fc425bddf2cb80ab6872b58a6d2ad88a
Pitching <adchir95@gmail.com>
2023-05-25 14:53:17 -0400

    adding the main wrapper for container

--- Motorent-client/src/components/Landing.js
+++ Motorent-client/src/components/Landing.js
@@ -3,18 +3,20 @@ import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Landing () {
+function Landing() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          example text
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <main>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            example text
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </main>
   );
 }
 
