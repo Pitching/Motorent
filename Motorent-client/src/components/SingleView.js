098ace8a209bc4ccfabafe66a9e6b30610d88ae6
Pitching <adchir95@gmail.com>
2023-06-04 15:01:23 -0400

    creating the src styling name

--- Motorent-client/src/components/SingleView.js
+++ Motorent-client/src/components/SingleView.js
@@ -7,6 +7,7 @@ function SingleView() {
         <Carousel.Item>
           <img 
             className="d-block w-100"
+            src=""
           />
         </Carousel.Item>
       </Carousel>
