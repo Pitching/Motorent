5e9e160a2b403a819738e4a11d49a2e0876d146e
Pitching <adchir95@gmail.com>
2023-06-16 15:06:41 -0400

    providing alt name for the carousel second slide img

--- Motorent-client/src/components/SingleView.js
+++ Motorent-client/src/components/SingleView.js
@@ -19,6 +19,7 @@ function SingleView() {
           <img 
             className="d-block w-100"
             src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
           />
         </Carousel.Item>
       </Carousel>
